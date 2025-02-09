8ad1409a7cc5ff6e1511fa8338f6202b959f817e
LoueyZormati <loueyzormati4@gmail.com>
2025-02-09 12:07:51 +0100

    pipeline file added

--- playwright.config.js
+++ playwright.config.js
@@ -1,34 +1,30 @@
-// @ts-check
 const { defineConfig, devices } = require('@playwright/test');
-const { on } = require('events');
 
-
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 module.exports = defineConfig({
   testDir: './TestCases',
- 
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
+  // Définir plusieurs projets pour différents navigateurs
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], browserName: 'chromium' },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'], browserName: 'firefox' },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'], browserName: 'webkit' },
+    },
+  ],
+
   use: {
-    browserName : 'chromium',
-    headless: false ,
-    video:'retain-on-failure',
-    screenshot : 'on' ,
-    trace:'retain-on-failure',
-    //viewport:{width:720,height:720}
-    
+    headless: false, // Exécuter en mode headful (visible) ou headless (invisible)
+    video: 'retain-on-failure',
+    screenshot: 'on',
+    trace: 'retain-on-failure',
   },
-
-  
- 
-
 });
-
--- playwright.service.config.js
+++ playwright.service.config.js
@@ -2,21 +2,15 @@ const { defineConfig } = require('@playwright/test');
 const { getServiceConfig, ServiceOS } = require('@azure/microsoft-playwright-testing');
 const config = require('./playwright.config');
 
-/* Learn more about service configuration at https://aka.ms/mpt/config */
 export default defineConfig(
   config,
   getServiceConfig(config, {
     exposeNetwork: '<loopback>',
     timeout: 30000,
     os: ServiceOS.LINUX,
-    useCloudHostedBrowsers: true // Utilisation des navigateurs hébergés dans le cloud.
+    useCloudHostedBrowsers: true, // Utilisation des navigateurs hébergés dans le cloud
   }),
   {
-    /* 
-    Playwright Testing service reporter is added by default.
-    This will override any reporter options specified in the base playwright config.
-    If you are using more reporters, please update your configuration accordingly.
-    */
     reporter: [['list'], ['@azure/microsoft-playwright-testing/reporter']],
   }
 );
